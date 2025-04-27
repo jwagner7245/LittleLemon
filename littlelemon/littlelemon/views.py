7a605db8f5beb1a5e1ccf26b75e3fce396e6f781
John Wagner <m1mac@Johns-MacBook-Pro.local>
2025-04-26 21:44:34 -0400

    added token auth

--- littlelemon/littlelemon/views.py
+++ littlelemon/littlelemon/views.py
@@ -4,7 +4,7 @@ from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIV
 from rest_framework.permissions import IsAuthenticated
 
 from restaurant.models import Menu, Booking
-from .serializers import BookingSerializer, MenuSerializer
+from restaurant.serializers import BookingSerializer, MenuSerializer
 
 
 class MenuItemView(ListCreateAPIView):
